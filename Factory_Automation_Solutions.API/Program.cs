38c84f6f3d1ccf145fb525383065ac8bf06d878b
AkbarovJahongir <akbarovdzahongir73@gmail.com>
2023-03-10 08:57:46 +0500

    Db connection

--- Factory_Automation_Solutions.API/Program.cs
+++ Factory_Automation_Solutions.API/Program.cs
@@ -1,12 +1,16 @@
 using Factory_Automation_Solutions.Application.Services;
 using Factory_Automation_Solutions.Domain.Models;
 using Factory_Automation_Solutions.Application.Common.Interfaces;
+using Factory_Automation_Solutions.Application;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddTransient<ISupplierService<Supplier>, SupplierService>(); ;
-builder.Services.AddControllers();
+builder.Services.AddTransient<ISupplierService, SupplierService>(); ;
+builder.Services.AddControllers(); 
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
